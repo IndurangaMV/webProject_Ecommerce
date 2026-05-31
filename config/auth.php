43eb3d687ea506c558736fa2bc407f9747845e9f
IndurangaMV <induranga21297@gmail.com>
2026-05-31 10:33:03 +0530

    database update

--- config/auth.php
+++ config/auth.php
@@ -5,17 +5,21 @@ require_once "connection.php";
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$sql="SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
 
-if($result = $conn->query($sql)){
-    if($result->num_rows > 0){
+if ($result = $conn->query($sql)) {
+    if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $_SESSION["user"] = $row["username"];
         $_SESSION["user_type"] = $row["user_type"];
-        header("Location: ../views/dashboard.php");
-        exit;
-    }else{
-        echo "Invalid Login";
+        if ($_SESSION["user_type"] == 3) {
+            header("Location: ../views/index.php");
+            exit;
+        } else if ($_SESSION["user_type"] == 1 || $_SESSION["user_type"] == 2) {
+            header("Location: ../views/dashboard.php");
+            exit;
+        } else {
+            echo "Invalid Login";
+        }
     }
 }
-?>
